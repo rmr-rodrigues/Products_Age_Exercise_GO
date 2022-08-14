726e3b62dc2360bc5cc90b06ada5e6ff53999fb7
Ricardo Rodrigues <ricardorodrigues.mfr@gmail.com>
2022-08-14 14:15:32 +0100

    Implemented TestFilterOrders correct path.

--- MainService_test.go
+++ MainService_test.go
@@ -1,15 +1,25 @@
 package Products_Age
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
 
-const datePattern string = "2006-01-02 15:04:05"
-
 // TestFilterOrders calls FilterOrders with a list of valid orders and valid dates
 func TestFilterOrders(t *testing.T) {
-	t1, _ := time.Parse(datePattern, "2022-08-13 00:00:00")
-	t2, _ := time.Parse(datePattern, "2021-08-30 00:00:00")
+	t1, _ := time.Parse(datePattern, "2018-01-01 00:00:00")
+	t2, _ := time.Parse(datePattern, "2019-01-01 00:00:00")
 
+	t.Run("filters 3 orders out of 5", func(t *testing.T) {
+		ordersList := []Order{order1, order2, order3, order4, order5}
+
+		want := []Order{order1, order3, order4}
+
+		got := FilterOrders(ordersList, t1, t2)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	})
 }
